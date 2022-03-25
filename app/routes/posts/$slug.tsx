155354eb3e1931cabe59f381f445f1c4534f6974
rldnrl <devpark1002@gmail.com>
2022-03-25 18:15:18 +0900

    complete

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,17 @@
+import { json, useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+import invariant from "tiny-invariant";
+
+import { getPost, Post } from "~/post";
+
+export const loader: LoaderFunction = async ({
+  params,
+}) => {
+  invariant(params.slug, "expected params.slug");
+  return json(await getPost(params.slug));
+};
+
+export default function PostSlug() {
+  const post = useLoaderData<Post>();
+  return <main dangerouslySetInnerHTML={{ __html: post.html }} />
+}
---
+++ posts/test slug.md
@@ -0,0 +1,6 @@
+---
+title: test title
+---
+
+# title
+## End Game
